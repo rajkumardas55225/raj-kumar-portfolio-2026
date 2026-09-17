26e05af9d017bc915fc7263c20f48333662448f8
rajkumardas55225 <rajk4881169@gmail.com>
2026-09-18 00:16:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -220,9 +220,14 @@ def sitemap():
 </urlset>"""
     return xml, 200, {'Content-Type': 'application/xml'}
 
+@app.route('/google24d3232dac5002f0.html')
+def google_html_verification():
+    return "google-site-verification: google24d3232dac5002f0.html", 200, {'Content-Type': 'text/html'}
+
 @app.route('/google-verification')
 def google_verification_check():
     return '<meta name="google-site-verification" content="ABp0Hm9K6b12oR3G-pghgfcPdTA1Y53U68bIUHk8C1w" />', 200, {'Content-Type': 'text/html'}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
